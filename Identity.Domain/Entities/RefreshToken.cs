aab7e69db4fb1aa50e42ac44bfa2df5e444b913c
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2025-12-30 07:55:05 +0700

    Create Entities Identity

---
+++ Identity.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int RefreshTokenId { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; } = null!;
+        public string JwtId { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RevokedAt { get; set; }
+        public bool IsRevoked { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
