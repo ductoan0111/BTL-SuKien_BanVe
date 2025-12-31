d27521bca21a1c85dcf1499aa352f611636d9ec4
DucToan Nguyen <ductoan.hy0111@gmail.com>
2025-12-31 21:44:49 +0700

    them admin

---
+++ EventManagement.Admin/Program.cs
@@ -0,0 +1,44 @@
+﻿using EventManagement.Admin.Data;
+using EventManagement.Admin.Repository;
+using EventManagement.Admin.Repository.Interfaces;
+using EventManagement.Admin.Service;
+using EventManagement.Admin.Service.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Gán chuỗi kết nối cho DataHelper (ADO.NET)
+DataHelper.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDanhMucSuKienRepository, DanhMucSuKienRepository>();
+builder.Services.AddScoped<IDiaDiemRepository, DiaDiemRepository>();
+builder.Services.AddScoped<IDanhMucSuKienService, DanhMucSuKienService>();
+builder.Services.AddScoped<IDiaDiemService, DiaDiemService>();
+builder.Services.AddScoped<INguoiDungRepository, NguoiDungRepository>();
+builder.Services.AddScoped<INguoiDungService, NguoiDungService>();
+builder.Services.AddScoped<IVaiTroRepository, VaiTroRepository>();
+builder.Services.AddScoped<IVaiTroService, VaiTroService>();
+builder.Services.AddScoped<ISuKienAdminRepository, SuKienAdminRepository>();
+builder.Services.AddScoped<ISuKienAdminService, SuKienAdminService>();
+builder.Services.AddScoped<IAdminReportRepository, AdminReportRepository>();
+builder.Services.AddScoped<IAdminReportService, AdminReportService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
