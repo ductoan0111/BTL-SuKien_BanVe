2e61b7c649d844b188dfe78b2d98e589248fb93f
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-02 13:46:46 +0700

    them models nguoi dung

---
+++ EventManagement.Admin/Models/NguoiDung.cs
@@ -0,0 +1,18 @@
+﻿namespace EventManagement.Admin.Models
+{
+    public class NguoiDung
+    {
+        public int NguoiDungID { get; set; }
+        public int VaiTroID { get; set; }
+        public string HoTen { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? SoDienThoai { get; set; }
+        public string? TenDangNhap { get; set; }
+        public string? MatKhauHash { get; set; }
+        public DateTime NgayTao { get; set; }
+        public bool TrangThai { get; set; }
+
+        // optional cho view
+        public string? TenVaiTro { get; set; }
+    }
+}
